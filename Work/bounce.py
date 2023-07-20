1141dc0ff54e68662fb44d8c4df5fdef23701acd
Jeff Beal <jbeal@ramp.com>
2023-07-20 11:34:04 -0700

    Exercise 1.5: The Bouncing Ball

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+
+ball_height = 100.0
+BOUNCE_RATE = 0.6
+
+for i in range(10):
+    ball_height = ball_height * BOUNCE_RATE
+    print(f"Ball bounced back to {round(ball_height,4)}")
