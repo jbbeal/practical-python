b49524f333083bf4dc5713b1576a1ccec18959eb
Jeff Beal <jeffbeal@outlook.com>
2023-08-20 12:06:57 -0700

    More tests

--- Work/test_stock.py
+++ Work/test_stock.py
@@ -4,9 +4,24 @@ from stock import Stock
 class TestStock(unittest.TestCase):
     def test_create(self):
         s = Stock('GOOG', 100, 490.1)
-        self.assertEquals(s.name, 'GOOG')
-        self.assertEquals(s.shares, 100)
-        self.assertEquals(s.price, 490.1)
+        self.assertEqual(s.name, 'GOOG')
+        self.assertEqual(s.shares, 100)
+        self.assertEqual(s.price, 490.1)
+
+    def test_cost(self):
+        s = Stock('MSFT', 53, 48.7)
+        self.assertEqual(s.cost, 53*48.7)
+
+    def test_sell(self):
+        s = Stock('AMZN', 100, 54.8)
+        s.sell(25)
+        self.assertEqual(s.shares, 75)
+        self.assertEqual(s.cost, 75 * 54.8)
+
+    def test_bad_shares(self):
+        s = Stock('JPM', 100, 47.8)
+        with self.assertRaises(TypeError):
+            s.shares = '150'
 
 if __name__ == '__main__':
     unittest.main()
